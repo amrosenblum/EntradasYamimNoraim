3b1de95e15533e289a3b84e347c62584dcf0dbb4
amrosenblum <amrosenblum@uc.cl>
2025-07-23 13:40:19 -0400

    feat: add react-router-dom for routing and update main.tsx for navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Gracias from './gracias'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <Gracias />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/gracias" element={<Gracias />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
