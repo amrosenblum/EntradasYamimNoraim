82c7affb7d1ee2a5cd7eece09419efc2b03ad289
amrosenblum <amrosenblum@uc.cl>
2025-07-23 14:24:36 -0400

    refactor: update .gitignore and modify API handler to use VercelRequest and VercelResponse

--- api/create-webpay.ts
+++ api/create-webpay.ts
@@ -1,12 +1,12 @@
 // /pages/api/create-webpay.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { VercelRequest, VercelResponse } from '@vercel/node'
 import { IntegrationApiKeys, WebpayPlus } from 'transbank-sdk';
 
 const wp: any = WebpayPlus;
 
 wp.configureForProduction('TU_CODIGO_COMERCIO', 'TU_API_KEY');
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') return res.status(405).end();
 
   const { buy_order, session_id, amount, return_url } = req.body;
